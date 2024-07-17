fa959a7d89d5eca3f9bd39d29a535b0975880260
Soufiane Amama <soufianeamama70@gmail.com>
2024-07-17 14:24:20 +0100

    اعداد اتصال WebSockets بين Server و Client

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const socket = require('socket.io');
 
 // App setup
 const app = express();
 const PORT = 4000;
 
+// listen for requests
+const server = app.listen(PORT, () => {
+  console.log('listening for requests on port', PORT)
+})
+
+// Socket setup & pass server
+const io = socket(server);
+
+io.on('connection', (socket) => { // حدث الاتصال
+  console.log('made socket connection', socket.id);
+});
+
+
 // Static files
 app.use(express.static('public')); // سيبحث في المجلد public عن ملف ثابت والذي ينتهي بالامتداد .html
 
-
-
-// listen for requests
-app.listen(PORT, () => {
-    console.log('listening for requests on port', PORT)
-  })
---
+++ public/chat.js
@@ -0,0 +1,4 @@
+// Make connection
+var socket = io.connect('http://localhost:4000'); // io نقوم بجلبها من مكتبة socket.io-client في ملف index.html
+
+
